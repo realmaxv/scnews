505d256096324127c3be4a6d235db5e1fe2c7ffc
realmaxv <max.vossgaetter@gmx.de>
2025-03-17 14:46:53 +0100

    all functions

--- src/main.ts
+++ src/main.ts
@@ -7,19 +7,14 @@ const userInputSearch = document.getElementById(
 ) as HTMLInputElement;
 
 const outputDiv = document.querySelector<HTMLDivElement>(".output");
-const inputLanguage = document.querySelector("#input-sprache") as HTMLSelectElement;
-const inputSort = document.querySelector("#input-sort") as HTMLSelectElement; 
+const inputLanguage = document.querySelector(
+  "#input-sprache"
+) as HTMLSelectElement;
+const inputSort = document.querySelector("#input-sort") as HTMLSelectElement;
 const submitBtn = document.querySelector("#search-btn") as HTMLButtonElement;
 
-
-
-let userValue = userInputSearch.value;
-type NewsInput = {
-  articles: string[];
-};
-
-function userSearch(input: string, inputUserLang: string) {
-  const searchLinkAPI = `https://newsapi.org/v2/everything?q=${input}&apiKey=${apiKey}&language=${inputUserLang}`;
+function userSearch(input: string, inputUserLang: string, sort: string) {
+  const searchLinkAPI = `https://newsapi.org/v2/everything?q=${input}&apiKey=${apiKey}&language=${inputUserLang}&sortBy=${sort}`;
 
   fetch(searchLinkAPI)
     .then((response) => response.json())
@@ -40,46 +35,16 @@ function userSearch(input: string, inputUserLang: string) {
 
         newArticle.append(newHeadline, description, image, articleBtn);
         if (outputDiv) {
-            outputDiv.appendChild(newArticle);
+          outputDiv.appendChild(newArticle);
         }
       });
     });
 }
+submitBtn.addEventListener("click", () => {
+  if (outputDiv) {
+    outputDiv.innerHTML = "";
+  }
 
-userSearch();
-
-inputLanguage.addEventListener("change", (input) => {
-    if(outputDiv){
-     outputDiv.innerHTML = "";
-    }
-    switch(inputLanguage.value) {
-        case "de": userSearch(input, "de")
-        break;
-        case "en": userSearch(input, "en")
-        break;
-        case "es": userSearch(input, "es")
-        break;
-        case "fr": userSearch(input, "fr")
-        break;
-        case "it": userSearch(input, "it")
-        break;
-        case "ru": userSearch(input, "ru")
-        break;
-        case "ar": userSearch(input, "ar")
-        break;
-        case "he": userSearch(input, "he")
-        break;
-        case "nl": userSearch(input, "nl")
-        break;
-        case "no": userSearch(input, "no")
-        break;
-        case "sv": userSearch(input, "sv")
-        break;
-        case "pt": userSearch(input, "pt")
-        break;
-        case "chn": userSearch(input, "chn")
-        break;
-        default:
-    }
-    }
-);
+  console.log(userInputSearch.value);
+  userSearch(userInputSearch.value, inputLanguage.value, inputSort.value);
+});
